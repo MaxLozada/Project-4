d7dc7faec764391ef6ff9f523f7f34d75cc618b3
MAXLO <ml644@njit.edu>
2022-05-13 04:57:25 -0400

    updated testing

--- tests/map_test.py
+++ tests/map_test.py
@@ -1,11 +1,25 @@
 """This test the map"""
 
 
-def test_request_locations_1(client):
+def test_request_locations(client):
     """This makes the index page"""
     response = client.get("/locations")
     assert response.status_code == 200
     assert b"locations" in response.data
+
+
+def test_request_locations_upload(client):
+    """This makes the index page"""
+    response = client.get("/locations/upload")
+    assert response.status_code == 200
+    assert b"locations/upload" in response.data
+
+
+def test_request_locations_upload(client):
+    """This makes the index page"""
+    response = client.get("/locations/new")
+    assert response.status_code == 200
+    assert b"locations/new" in response.data
 
 
 def test_request_locations_map(client):
