3ffd93ca1689c4de8b9ec2b5e92d90a693109312
MAXLO <ml644@njit.edu>
2022-05-13 03:59:34 -0400

    updated testing

--- tests/map_test.py
+++ tests/map_test.py
@@ -1,7 +1,7 @@
 """This test the map"""
 
 
-def test_request_main_menu_links(client):
+def test_request_locations(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
@@ -9,16 +9,16 @@ def test_request_main_menu_links(client):
     assert b'href="/locations/map"' in response.data
 
 
-def test_request_about(client):
+def test_request_locations_1(client):
     """This makes the index page"""
     response = client.get("/locations")
     assert response.status_code == 200
     assert b"locations" in response.data
 
 
-def test_request_page1(client):
+def test_request_locations_map(client):
     """This makes the index page"""
     response = client.get("/locations/map")
     assert response.status_code == 200
-    assert b"map" in response.data
+    assert b"locations/map" in response.data
 
